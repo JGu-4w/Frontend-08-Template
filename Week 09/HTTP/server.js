4b55b8084296fbad8eb69942f1e22678320b798f
JGu-4w <joegu.4work@gmail.com>
2021-02-23 00:18:13 +0800

    feat(*): HTML解析标签

---
+++ Week 09/HTTP/server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  console.log("request received");
+  console.log(request.headers);
+  response.setHeader('Content-Type', 'text/html');
+  response.setHeader('X-Foo', 'bar');
+  response.writeHead(200, { 'Content-Type': 'text/plain'});
+  response.end(
+`<html maaa=a>
+<head>
+    <style>
+body div #myid{
+  width: 100px;
+  background-color: #ccc;
+}
+    </style>
+</head>
+<body>
+  <div>
+    <img id="myid"/>
+    <img />
+  </div>
+</body>
+</html>`);
+});
+
+server.listen(8088);
