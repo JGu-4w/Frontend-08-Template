ae0f508bed6dca126f5ffe979deacfe04006f22d
JGu-4w <joegu.4work@gmail.com>
2021-02-23 21:16:00 +0800

    feat(*): 创建元素和处理属性

--- Week 09/HTTP/parser.js
+++ Week 09/HTTP/parser.js
@@ -1,11 +1,24 @@
+let currentToken = null;
+let currentAttribute = null;
+
+function emit(token) {
+  console.log(token);
+}
+
 const EOF = Symbol('EOF'); // EOF: End Of File
 
 function data(c) {
   if (c === "<") {
     return tagOpen;
   } else if (c === EOF) {
-    return ;
+    emit({
+      type: "EOF"
+    });
   } else {
+    emit({
+      type: "text",
+      content: c
+    });
     return data;
   }
 }
@@ -14,6 +27,10 @@ function tagOpen(c) {
   if (c === "/") {
     return endTagOpen;
   } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "startTag",
+      tagName: ""
+    }
     return tagName(c);
   } else {
     return ;
@@ -22,6 +39,10 @@ function tagOpen(c) {
 
 function endTagOpen(c) {
   if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "endTag",
+      tagName: ""
+    }
     return tagName(c);
   } else if(c === ">") {
 
@@ -38,6 +59,7 @@ function tagName(c) {
   } else if (c === "/") {
     return selfClosingStartTag;
   } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken.tagName += c;
     return tagName;
   } else if (c === '>') {
     return data;
@@ -49,12 +71,113 @@ function tagName(c) {
 function beforeAttributeName(c) {
   if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName;
-  } else if (c === ">") {
-    return data;
+  } else if (c === ">" || c === '/' || c ===EOF) {
+    return afterAttributeName(c);
   } else if (c === "=") {
-    return beforeAttributeName;
+
   } else {
+    currentAttribute = {
+      name: "",
+      value: ""
+    }
+    return attributeName(c);
+  }
+}
+
+function attributeName(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === "/" || c === ">" || c === EOF) {
+    return afterAttributeName(c);
+  } else if (c === "=") {
+    return beforeAttributeValue;
+  } else if (c === "\u0000") {
+
+  } else if (c === "\"" || c === "'" || c === "<") {
+
+  } else {
+    currentAttribute.name += c;
+    return attributeName;
+  }
+}
+
+function beforeAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === "/" || c === ">" || c === EOF) {
+    return beforeAttributeValue;
+  } else if (c === "\"") {
+    return doubleQuotedAttributeValue;
+  } else if (c === "\'") {
+    return singleQuotedAttributeValue;
+  } else if (c === ">") {
+
+  } else {
+    return UnquotedAttributeValue(c);
+  }
+}
+
+function doubleQuotedAttributeValue(c) {
+  if (c === "\"") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttribureValue;
+  } else if (c === "\u0000") {
+
+  } else if (c === EOF) {
+    
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function singleQuotedAttributeValue(c) {
+  if (c === "\'") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return afterQuotedAttribureValue;
+  } else if (c === "\u0000") {
+
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c;
+    return singleQuotedAttributeValue;
+  }
+}
+
+function afterQuotedAttribureValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName;
+  } else if (c === "/") {
+    return selfClosingStartTag;
+  } else if (c === ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c;
+    return doubleQuotedAttributeValue;
+  }
+}
+
+function UnquotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return beforeAttributeName;
+  } else if (c === "/") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return selfClosingStartTag;
+  } else if (c === ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === "\u0000") {
+
+  } else if (c === "\"" || c === "'" || c === "<" || c === "=" || c === "`") {
+
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c;
+    return UnquotedAttributeValue;
   }
 }
 
