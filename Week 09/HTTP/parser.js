02c72802b178703c8b04dcbc549afdec76810f1a
JGu-4w <joegu.4work@gmail.com>
2021-02-28 22:30:06 +0800

    feat(*): specificity的计算逻辑

--- Week 09/HTTP/parser.js
+++ Week 09/HTTP/parser.js
@@ -39,6 +39,34 @@ function match(element, selector) {
   return false;
 }
 
+function specificity(selector) {
+  let p = [0, 0, 0, 0];
+  let selectorParts = selector.split(" ");
+  for (let part of selectorParts) {
+    if (part.charAt(0) === "#") {
+      p[1] += 1;
+    } else if (part.charAt(0) == ".") {
+      p[2] += 1;
+    } else {
+      p[3] += 1;
+    }
+  }
+  return p;
+}
+
+function compare(sp1, sp2) {
+  if (sp1[0] - sp2[0]) {
+    return sp1[0] - sp2[0];
+  }
+  if (sp1[1] - sp2[1]) {
+    return sp1[1] - sp2[1];
+  }
+  if (sp1[2] - sp2[2]) {
+    return sp1[2] - sp2[2];
+  }
+  return sp1[3] - sp2[3];
+}
+
 function computeCSS(element) {
   let elements = stack.slice().reverse()
   if (!element.computedStyle) {
@@ -67,11 +95,18 @@ function computeCSS(element) {
 
     if (matched) {
       // 如果匹配到，需要加入
+      let sp = specificity(rule.selectors[0]);
       let computedStyle = element.computedStyle;
       for (let declaration of rule.declarations) {
         if (!computedStyle[declaration.property]) {
           computedStyle[declaration.property] = {}
-          computedStyle[declaration.property].value = declaration.value;
+          if (!computedStyle[declaration.property].specificity) {
+            computedStyle[declaration.property].value = declaration.value;
+            computedStyle[declaration.property].specificity = sp;
+          } else if (compare(computedStyle[declaration.property].specificity, sp) < 0) {
+            computedStyle[declaration.property].value = declaration.value;
+            computedStyle[declaration.property].specificity = sp;
+          }
         }
       }
       console.log(element.computedStyle);
